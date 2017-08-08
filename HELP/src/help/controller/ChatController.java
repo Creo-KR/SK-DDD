e02be12028e7eb0289706356c3165430b3fcaf9a
user <user@user-PC>
2017-08-08 14:02:17 +0900

    임포트 수정

--- HELP/src/help/controller/ChatController.java
+++ HELP/src/help/controller/ChatController.java
@@ -3,6 +3,7 @@ package help.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
